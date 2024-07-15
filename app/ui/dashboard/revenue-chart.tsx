e57d84c9239e2410ff486c4e3d592d85bb552958
Giancarlos Sanchez <netunia811@gmail.com>
2024-07-15 13:36:33 -0700

    actualizacion

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,17 +1,15 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+import { fetchRevenue } from '@/app/lib/data';
 
 
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
-  const chartHeight = 350;
+export default async function RevenueChart() {
   
+  const revenue = await fetchRevenue();
+  
+  const chartHeight = 350;
 
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
